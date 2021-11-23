6426d86887a0b4e07f4739fbfafd70a2a7cfa84c
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2021-11-24 02:58:53 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+
+from riotwatcher import LolWatcher, ApiError
+
+st.title('韩服查询程序')
+api = st.text_input('请输入Riot API KEY:')
+summoner_name = st.text_input('请输入韩服ID')
+
+my_api = api
+
+# 创建一个lol_watcher对象
+lol_watcher = LolWatcher(my_api)
+
+# 玩家的信息
+region = 'kr' # Riot API开发文档里有写到各个服务器的region缩写
+
+# 创建一个lol_watcher下的summoner对象
+summoner = lol_watcher.summoner.by_name(region, summoner_name)
+
+puuid = summoner['puuid']
+
+# 提取matchlist
+matchlist = lol_watcher.match.matchlist_by_puuid(region='asia', puuid=puuid, type='ranked', count=50)
+
+list = []
+for i in matchlist:
+    list.append(lol_watcher.match.by_id('asia', i)['info']['gameEndTimestamp'])
+
+# 把UNIX时间戳转换成datetime
+
+import time
+from datetime import datetime
+
+def timestamp_to_strtime(timestamp):
+    #local_str_time = datetime.fromtimestamp(timestamp / 1000.0).strftime('%Y-%m-%d %H:%M')
+    local_str_time = datetime.fromtimestamp(timestamp / 1000.0).strftime('%Y-%m-%d')
+    return local_str_time
+
+time = []
+for i in list:
+    time.append(timestamp_to_strtime(i))
+
+time = pd.DataFrame(time, columns = ['Count'])
+
+st.table(time['Count'].value_counts().sort_index(ascending=False))
