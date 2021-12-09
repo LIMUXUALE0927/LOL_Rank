7b42a9ad4addf4d5db2afd52876ee41e09a03c50
Hao Li <50431940+LIMUXUALE0927@users.noreply.github.com>
2021-12-10 02:15:53 +0800

    Update LeaguePoints.py

--- LeaguePoints.py
+++ LeaguePoints.py
@@ -2,13 +2,13 @@ import streamlit as st
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 
-st.title('韩服分数查询程序')
-st.write('在下方文本框中输入要查询的韩服ID，ID之间用英文逗号分隔，最后一个ID之后不要加逗号')
+st.title('韩服排位分数批量查询程序')
+st.write('在下方文本框中输入要查询的韩服ID列表，注意各个ID之间要用英文逗号(,)分隔，但最后一个ID之后不要加逗号。')
 
 lol_watcher = LolWatcher(st.secrets["my_api"])
 region = 'kr'
 
-txt = st.text_area('IDs to analyze',
+txt = st.text_area('IDs to search',
                    '''
 BRO Morgan, KT Harp, chenzebin, MIDGOD 12
 '''
